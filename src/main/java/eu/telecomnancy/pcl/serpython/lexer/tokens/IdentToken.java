c971aaaa6a71793744a669f22318ab906e5e7d09
FuseTim <fusetim@gmx.com>
2024-10-27 17:57:25 +0100

    feat(lexer): Missing token for identifiers

---
+++ src/main/java/eu/telecomnancy/pcl/serpython/lexer/tokens/IdentToken.java
@@ -0,0 +1,31 @@
+package eu.telecomnancy.pcl.serpython.lexer.tokens;
+
+import eu.telecomnancy.pcl.serpython.common.Span;
+
+/**
+ * The IdentToken class is an abstract class representing a identifier token in the lexer.
+ * It extends the LitteralToken class and provides a name field to store the identifier.
+ */
+public abstract class IdentToken extends LitteralToken {
+    private String name;
+    /**
+     * Constructs a new IdentToken with the specified span.
+     *
+     * @param span the span of the token
+     */
+    public IdentToken(String name, Span span) {
+        super(span);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "IdentToken{" +
+                "name='" + name + '\'' +
+                ", span=" + span.toString() +
+                '}';
+    }
+}
