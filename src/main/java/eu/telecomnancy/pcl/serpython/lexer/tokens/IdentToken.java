9cc15f80aaed7fe29d1541121ba799f20777c884
FuseTim <fusetim@gmx.com>
2024-11-01 17:18:15 +0100

    fix(lexer): IdentToken should not be abstract

--- src/main/java/eu/telecomnancy/pcl/serpython/lexer/tokens/IdentToken.java
+++ src/main/java/eu/telecomnancy/pcl/serpython/lexer/tokens/IdentToken.java
@@ -3,10 +3,10 @@ package eu.telecomnancy.pcl.serpython.lexer.tokens;
 import eu.telecomnancy.pcl.serpython.common.Span;
 
 /**
- * The IdentToken class is an abstract class representing a identifier token in the lexer.
+ * The IdentToken class is an class representing a identifier token in the lexer.
  * It extends the LitteralToken class and provides a name field to store the identifier.
  */
-public abstract class IdentToken extends LitteralToken {
+public class IdentToken extends LitteralToken {
     private String name;
     /**
      * Constructs a new IdentToken with the specified span.
